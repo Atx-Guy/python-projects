58a4d7f9a9e3c0939506951877136c04f482aaa4
atxxx <atxxx@duck.com>
2025-03-10 22:20:14 -0500

    Initial commit

---
+++ convert-it/my-conda-env/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/home/ryan/python-projects/convert-it/my-conda-env/lib"
+XSLT_LIBS="-lxslt -L/home/ryan/python-projects/convert-it/my-conda-env/lib -lxml2 "
+XSLT_PRIVATE_LIBS="-lm"
+XSLT_INCLUDEDIR="-I/home/ryan/python-projects/convert-it/my-conda-env/include"
+MODULE_VERSION="xslt-1.1.39"
